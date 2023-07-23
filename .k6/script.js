846c8d5f2e6e5da71056dadb68e618c497e5dc45
Eloi Riera <hoquei0021@gmail.com>
2023-07-23 18:42:30 +0200

    feat: adding k6 and running self hosted runner

---
+++ .k6/script.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+export const options = {
+    stages: [
+        { duration: '30s', target: 100 },
+        { duration: '1m30s', target: 500 },
+        { duration: '20s', target: 0 },
+    ],
+};
+export default function () {
+    const res = http.get('https://msrk.junkers21.ovh/');
+    check(res, { 'status was 200': (r) => r.status == 200 });
+    sleep(1);
+}
